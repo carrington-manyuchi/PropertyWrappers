d87b425025a17b33fdb37d10d17dbea0f005770d
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-03-29 16:11:17 +0200

    Initial Commit

---
+++ PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by Manyuchi, Carrington C on 2025/03/29.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
